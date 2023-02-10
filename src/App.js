15ff8db67d42f79ef951aaef8b25bdeae85c4bd2
Zedkaere <zakariaaslimani1080@gmail.com>
2023-02-10 11:39:23 +0100

    modification titre 1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import desert from './desert.jpg';
 function App() {
   return (
     <div className="App">
-      <h1>Bienvenue au cfitech</h1>
+      <h1>Bienvenue</h1>
       <img src={desert} alt='Le desert' width={600} />
     </div>
   );
